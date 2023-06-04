c7e6cfe8133e4a007c90956b6bfa157184ac1dbb
Irina <myr-irina2@yandex.ru>
2023-06-05 00:11:04 +0300

    fix: fix errors after 1st review

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -15,7 +15,7 @@ import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import AppHeader from '../app-header/app-header';
 import styles from './styles.module.scss';
 
-import { useDispatch } from '../../services/hooks';
+import { useDispatch, useSelector } from '../../services/hooks';
 import { ProtectedRoute } from '../protected-routes/protected-routes';
 import { fetchIngredients } from '../../services/actions/ingredients';
 import { checkUserAuth } from '../../services/actions/user';
@@ -30,6 +30,7 @@ function App() {
   const background = location.state && location.state.background;
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { ingredientsRequest } = useSelector((store) => store.ingredients);
 
   const handleCloseModal = useCallback(() => {
     navigate(-1);
@@ -38,139 +39,143 @@ function App() {
   useEffect(() => {
     dispatch(fetchIngredients());
     dispatch(checkUserAuth());
-  }, [dispatch]);
+  }, []);
 
   return (
     <>
       <AppHeader />
 
-      <div className={styles.app}>
-        <Routes location={background || location}>
-          <Route path='/' element={<Main />} />
-          <Route
-            path='/login'
-            element={
-              <ProtectedRoute onlyUnAuth={true}>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/register'
-            element={
-              <ProtectedRoute onlyUnAuth={true}>
-                <Register />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/forgot-password'
-            element={
-              <ProtectedRoute onlyUnAuth={true}>
-                <ForgotPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/reset-password'
-            element={
-              <ProtectedRoute onlyUnAuth={true}>
-                <ResetPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/profile'
-            element={
-              <ProtectedRoute onlyUnAuth={false}>
-                <ProfileLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path='' element={<Profile />} />
+      {ingredientsRequest ? (
+        <h2>Загрузка..</h2>
+      ) : (
+        <div className={styles.app}>
+          <Routes location={background || location}>
+            <Route path='/' element={<Main />} />
             <Route
-              path='orders'
+              path='/login'
+              element={
+                <ProtectedRoute onlyUnAuth={true}>
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/register'
+              element={
+                <ProtectedRoute onlyUnAuth={true}>
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/forgot-password'
+              element={
+                <ProtectedRoute onlyUnAuth={true}>
+                  <ForgotPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/reset-password'
+              element={
+                <ProtectedRoute onlyUnAuth={true}>
+                  <ResetPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile'
+              element={
+                <ProtectedRoute onlyUnAuth={false}>
+                  <ProfileLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path='' element={<Profile />} />
+              <Route
+                path='orders'
+                element={
+                  <ProtectedRoute onlyUnAuth={false}>
+                    <ProfileOrders />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+
+            <Route
+              path='/profile/orders'
               element={
                 <ProtectedRoute onlyUnAuth={false}>
                   <ProfileOrders />
                 </ProtectedRoute>
               }
             />
-          </Route>
 
-          <Route
-            path='/profile/orders'
-            element={
-              <ProtectedRoute onlyUnAuth={false}>
-                <ProfileOrders />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path='/profile/orders/:id'
-            element={
-              <ProtectedRoute onlyUnAuth={false}>
-                <FeedOrderDetails />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route path='/feed'>
-            <Route path='' element={<Feed />} />
-
-            <Route
-              path=':id'
-              element={
-                <div className={styles.feed_wrapper}>
-                  <FeedOrderDetails />
-                </div>
-              }
-            />
-          </Route>
-
-          <Route path='/ingredients/:id' element={<Ingredient />} />
-
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-
-        {background && (
-          <Routes>
-            <Route
-              path='/ingredients/:id'
-              element={
-                <Modal
-                  title='Детали ингредиента'
-                  handleClose={handleCloseModal}
-                >
-                  <IngredientDetails />
-                </Modal>
-              }
-            />
-
-            <Route
-              path='/feed/:id'
-              element={
-                <div>
-                  <Modal title='' handleClose={handleCloseModal}>
-                    <FeedOrderDetails />
-                  </Modal>
-                </div>
-              }
-            />
             <Route
               path='/profile/orders/:id'
               element={
-                <div>
-                  <Modal title='' handleClose={handleCloseModal}>
-                    <FeedOrderDetails />
-                  </Modal>
-                </div>
+                <ProtectedRoute onlyUnAuth={false}>
+                  <FeedOrderDetails />
+                </ProtectedRoute>
               }
             />
+
+            <Route path='/feed'>
+              <Route path='' element={<Feed />} />
+
+              <Route
+                path=':id'
+                element={
+                  <div className={styles.feed_wrapper}>
+                    <FeedOrderDetails />
+                  </div>
+                }
+              />
+            </Route>
+
+            <Route path='/ingredients/:id' element={<Ingredient />} />
+
+            <Route path='*' element={<NotFound />} />
           </Routes>
-        )}
-      </div>
+
+          {background && (
+            <Routes>
+              <Route
+                path='/ingredients/:id'
+                element={
+                  <Modal
+                    title='Детали ингредиента'
+                    handleClose={handleCloseModal}
+                  >
+                    <IngredientDetails />
+                  </Modal>
+                }
+              />
+
+              <Route
+                path='/feed/:id'
+                element={
+                  <div>
+                    <Modal title='' handleClose={handleCloseModal}>
+                      <FeedOrderDetails />
+                    </Modal>
+                  </div>
+                }
+              />
+              <Route
+                path='/profile/orders/:id'
+                element={
+                  <div>
+                    <Modal title='' handleClose={handleCloseModal}>
+                      <FeedOrderDetails />
+                    </Modal>
+                  </div>
+                }
+              />
+            </Routes>
+          )}
+        </div>
+      )}
     </>
   );
 }
--- src/utils/api-requests.ts
+++ src/utils/api-requests.ts
@@ -25,188 +25,185 @@ export const WS_ORDERS_ALL: string =
 export const WS_ORDERS_PROFILE_URL: string =
   'wss://norma.nomoreparties.space/orders';
 
-export const checkResponse = <T>(res: TResponse<T>): Promise<T> => {
-  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
-};
-
-function requestUrl(url: string, options: RequestInit) {
-  return fetch(url, options).then(checkResponse);
-}
-
-export const getIngredients = () => {
-  return requestUrl(`${BASE_URL}/ingredients`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((data) => {
-    if (data?.success) {
-      return data;
-    }
-    return Promise.reject(data);
-  });
-};
-
-export const createOrder = (ingredients: Array<string | null>) => {
-  return requestUrl(`${BASE_URL}/orders`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: 'Bearer ' + getCookie('accessToken'),
-    },
-    body: JSON.stringify({
-      ingredients: ingredients,
-    }),
-  });
-};
-
-export const resetPasswordRequest = (email: string) => {
-  return requestUrl(`${BASE_URL}/password-reset`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email: email }),
-  });
-};
-
-export const createNewPasswordRequest = (formData: TFormData) => {
-  return requestUrl(`${BASE_URL}/password-reset/reset`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify({
-      password: formData.password,
-      token: formData.token,
-    }),
-  });
-};
-
-export const registerRequest = (userData: TUser) => {
-  return requestUrl(`${BASE_URL}/auth/register`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
-
-export const refreshToken = () => {
-  return fetch(`${BASE_URL}/auth/token`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify({
-      token: localStorage.getItem('refreshToken'),
-    }),
-  }).then(checkResponse);
-};
-
-export const fetchWithRefresh = async (url: string, options: RequestInit) => {
-  try {
-    const res = await fetch(url, options);
-
-    return await checkResponse(res);
-  } catch (err) {
-    if (err instanceof Error) {
-      if (err.message === 'jwt expired') {
-        const refreshData = await refreshToken();
-        console.log({ refreshData });
-
-        if (!refreshData.success) {
-          return Promise.reject(refreshData);
-        }
-        localStorage.setItem('refreshToken', refreshData.refreshToken);
-        let authToken;
-
-        if (refreshData.accessToken.indexOf('Bearer') === 0) {
-          authToken = refreshData.accessToken.split('Bearer ')[1];
-        }
-
-        if (authToken) {
-          setCookie('accessToken', authToken);
-        }
-
-        if (options.headers) {
-          (options.headers as { [key: string]: string }).Authorization =
-            'Bearer ' + authToken;
-        }
-
-        const res = await fetch(url, options);
-        return await checkResponse(res);
-      }
-    } else {
-      return Promise.reject(err);
-    }
+  export const checkResponse = <T>(res: TResponse<T>): Promise<T> => {
+    return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+  };
+  
+  function requestUrl(url: string, options: RequestInit) {
+    return fetch(url, options).then(checkResponse);
   }
-};
-
-export const getUser = () => {
-  return fetchWithRefresh(`${BASE_URL}/auth/user`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + getCookie('accessToken'),
-    },
-  }).then((data) => {
-    if (data?.success) {
-      return data;
+  
+  export const getIngredients = () => {
+    return requestUrl(`${BASE_URL}/ingredients`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((data) => {
+      if (data?.success) {
+        return data;
+      }
+      return Promise.reject(data);
+    });
+  };
+  
+  export const createOrder = (ingredients: Array<string | null>) => {
+    return requestUrl(`${BASE_URL}/orders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + getCookie('accessToken'),
+      },
+      body: JSON.stringify({
+        ingredients: ingredients,
+      }),
+    });
+  };
+  
+  export const resetPasswordRequest = (email: string) => {
+    return requestUrl(`${BASE_URL}/password-reset`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email }),
+    });
+  };
+  
+  export const createNewPasswordRequest = (formData: TFormData) => {
+    return requestUrl(`${BASE_URL}/password-reset/reset`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        password: formData.password,
+        token: formData.token,
+      }),
+    });
+  };
+  
+  export const registerRequest = (userData: TUser) => {
+    return requestUrl(`${BASE_URL}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  export const refreshToken = () => {
+    return fetch(`${BASE_URL}/auth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem('refreshToken'),
+      }),
+    }).then(checkResponse);
+  };
+  
+  export const fetchWithRefresh = async (url: string, options: RequestInit) => {
+    try {
+      const res = await fetch(url, options);
+  
+      return await checkResponse(res);
+    } catch (err) {
+        if ((err as { message: string; }).message === 'jwt expired') {
+          const refreshData = await refreshToken();
+  
+          if (!refreshData.success) {
+            return Promise.reject(refreshData);
+          }
+          localStorage.setItem('refreshToken', refreshData.refreshToken);
+          let authToken;
+  
+          if (refreshData.accessToken.indexOf('Bearer') === 0) {
+            authToken = refreshData.accessToken.split('Bearer ')[1];
+          }
+  
+          if (authToken) {
+            setCookie('accessToken', authToken);
+          }
+  
+          if (options.headers) {
+            (options.headers as { [key: string]: string }).authorization =  'Bearer ' + authToken;
+          }
+  
+          const res = await fetch(url, options);
+          return await checkResponse(res);
+      } else {
+        return Promise.reject(err);
+      }
     }
-    return Promise.reject(data);
-  });
-};
-
-export const loginRequest = (form: TUserLogin) => {
-  return requestUrl(`${BASE_URL}/auth/login`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(form),
-  });
-};
-
-export const logoutRequest = () => {
-  return requestUrl(`${BASE_URL}/auth/logout`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ token: localStorage.getItem('refreshToken') }),
-  });
-};
-
-export const updateUserRequest = ({ email, name, password }: TUserUpdate) => {
-  return fetchWithRefresh(`${BASE_URL}/auth/user`, {
-    method: 'PATCH',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + getCookie('accessToken'),
-    },
-    body: JSON.stringify({ email, name, password }),
-  }).then((data) => {
-    if (data?.success) {
-      return data;
-    }
-    return Promise.reject(data);
-  });
-};
-
-export const getBurgerOrderData = (orderNumber: string) => {
-  return requestUrl(`${BASE_URL}/orders/${orderNumber}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
+  };
+  
+  export const getUser = () => {
+    return fetchWithRefresh(`${BASE_URL}/auth/user`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + getCookie('accessToken'),
+      },
+    }).then((data) => {
+      if (data?.success) {
+        return data;
+      }
+      return Promise.reject(data);
+    });
+  };
+  
+  export const loginRequest = (form: TUserLogin) => {
+    return requestUrl(`${BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    });
+  };
+  
+  export const logoutRequest = () => {
+    return requestUrl(`${BASE_URL}/auth/logout`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token: localStorage.getItem('refreshToken') }),
+    });
+  };
+  
+  export const updateUserRequest = ({ email, name, password }: TUserUpdate) => {
+    return fetchWithRefresh(`${BASE_URL}/auth/user`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + getCookie('accessToken'),
+      },
+      body: JSON.stringify({ email, name, password }),
+    }).then((data) => {
+      if (data?.success) {
+        return data;
+      }
+      return Promise.reject(data);
+    });
+  };
+  
+  export const getBurgerOrderData = (orderNumber: string) => {
+    return requestUrl(`${BASE_URL}/orders/${orderNumber}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
