3f875949ff5356d5dfdc4cd69111d5870e6ab426
Irina <myr-irina2@yandex.ru>
2023-06-04 18:46:28 +0300

    feat: add profile orders

--- src/components/profile-orders/profile-orders.tsx
+++ src/components/profile-orders/profile-orders.tsx
@@ -7,23 +7,34 @@ import {
   wsProfileStart,
 } from '../../services/actions/ws-profile';
 import { WS_ORDERS_PROFILE_URL } from '../../utils/api-requests';
+import OrderCard from '../order-card/order-card';
+import Preloader from '../preloader/preloader';
 
 function ProfileOrders() {
   const dispatch = useDispatch();
   const accessToken = getCookie('accessToken');
 
   const privateOrders = useSelector(profileOrders);
-  console.log({ accessToken, privateOrders });
 
   useEffect(() => {
     dispatch(wsProfileStart(`${WS_ORDERS_PROFILE_URL}?token=${accessToken}`));
 
     return () => {
-      dispatch(wsProfileClosed);
+      dispatch(wsProfileClosed());
     };
   }, [dispatch, accessToken]);
 
-  return <div>ProfileOrders</div>;
+  console.log({ accessToken, privateOrders });
+
+  if (!privateOrders) return <Preloader />;
+
+  return (
+    <>
+      {privateOrders.map((order, index) => {
+        return <OrderCard order={order} key={index} isProfile={true} />;
+      })}
+    </>
+  );
 }
 
 export default ProfileOrders;
